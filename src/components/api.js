6faea1e80f35d0eee981ce363128f6731e578186
regina gimutao <gimutaoregina191@gmail.com>
2023-09-13 16:59:06 +0800

    daily weather forecast

---
+++ src/components/api.js
@@ -0,0 +1,13 @@
+export const geoAPIOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '0f73748e6bmsh1e13e8bf8106ddap1c22fcjsn4d982faf3c20',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+
+export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
+
+export const WEATHER_API_KEY = 'a7111b33d951fb7b5020641c6d2f0026';
